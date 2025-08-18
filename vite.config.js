b5e45a57ba81aef3e8ca16d6589330cf491326e3
Jung-Ho Um <jhum@kisti.re.kr>
2025-08-18 18:20:19 +0900

    deployment modification

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://sunujh6.github.io/vibecode-break-bricks/',
   plugins: [react()],
+  build: {outDir: 'dist', sourcemap: true },	
 })
